1de4f55102f2c3cea09fac19e989dc1027acf492
lilalr <gurennut@gmail.com>
2025-10-24 15:43:49 +0700

    membuat text untuk univ

--- app/src/main/java/com/example/questadvancelayout_041/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_041/UIPertama.kt
@@ -38,6 +38,11 @@ fun ActivitasPertama(modifier: Modifier){
             fontWeight = FontWeight.Bold
         )
 
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 18.sp
+        )
+
 
                     )
                 }
