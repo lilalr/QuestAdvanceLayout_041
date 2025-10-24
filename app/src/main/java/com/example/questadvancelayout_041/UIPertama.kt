ea3919119f1dfef229d77892ca38c57a55bbf21e
lilalr <gurennut@gmail.com>
2025-10-24 15:41:02 +0700

    membuat column awal

--- app/src/main/java/com/example/questadvancelayout_041/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_041/UIPertama.kt
@@ -27,6 +27,11 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun ActivitasPertama(modifier: Modifier){
+    Column(modifier= Modifier
+        .padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
                     )
                 }
